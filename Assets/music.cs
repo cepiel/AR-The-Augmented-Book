84c87eb8ca43a77a841c8983a75c1dd2d0c539b1
cepiel <lucas@cepiel.net>
2019-09-13 11:35:29 -0500

    Initial commit. Project already completed

---
+++ Assets/music.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+
+public class music : MonoBehaviour, ITrackableEventHandler
+{
+    private AudioSource ambience;
+    private TrackableBehaviour mTrackableBehaviour;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //ambience.GameObject.Find("musicTrack").GetComponent<AudioSource>();
+        
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if(mTrackableBehaviour){
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTrackableStateChanged( 
+        TrackableBehaviour.Status previousStatus,
+        TrackableBehaviour.Status newStatus)
+        {
+            if(newStatus == TrackableBehaviour.Status.DETECTED ||
+                newStatus == TrackableBehaviour.Status.TRACKED ||
+                newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+                {
+                    GetComponent<AudioSource>().Play();
+                    //ambience.Play();
+                }
+                else
+                {
+                    {
+                        GetComponent<AudioSource>().Stop();
+                        //ambience.Stop();
+                    }
+                }
+        }
+}
---
+++ Assets/vb_anim.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class vb_anim : MonoBehaviour, IVirtualButtonEventHandler
+{
+
+    public GameObject vbBtnObj;
+    public Animator ani;
+    public AudioClip testTrack;
+    AudioSource aud;
+    public bool on = false;
+
+     // Start is called before the first frame update
+    void Start()
+    {
+        vbBtnObj = GameObject.Find("btn");
+        vbBtnObj.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+        ani.GetComponent<Animator>();
+        
+        aud = GetComponent<AudioSource>();
+        testTrack.LoadAudioData();
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb) {
+        if(on == false){
+            on = true;
+            ani.Play("speaker_anim");
+            aud.PlayOneShot(testTrack, 2F);
+        }
+        else
+        {
+            ani.Play("none");
+            on = false;
+        }
+       
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb) {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
